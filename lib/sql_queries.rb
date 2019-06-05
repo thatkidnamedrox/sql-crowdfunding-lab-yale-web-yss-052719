77710f1cbd3103c944dbbeb5aeee7720fd053008
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 15:14:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   SELECT projects.title, pledges.amount
   FROM projects
   INNER JOIN pledges
-  WHERE project.id = pledges.project_id
+  WHERE projects.id = pledges.project_id
   """
 end
 
