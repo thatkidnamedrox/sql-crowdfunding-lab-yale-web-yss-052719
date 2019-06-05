8917c24bb65fe12e9c833e48f779df0a390660b1
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 15:41:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,7 +37,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   INNER JOIN pledges
   ON projects.id = pledges.project_id
   GROUP BY projects.title
-  HAVING SUM(pledges.amount) > PROJECTS
+  HAVING SUM(pledges.amount) >= projects.funding_goal
 
   ;
   """
