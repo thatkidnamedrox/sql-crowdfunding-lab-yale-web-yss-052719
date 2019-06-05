103d4bcdcaff3e6d36e53484fa7fefd8e8396f6c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 15:14:15 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -13,5 +13,14 @@ CREATE TABLE users
 (
   id INTEGER PRIMARY KEY,
   name TEXT,
-  age INTEGER,
+  age INTEGER
+);
+
+
+CREATE TABLE pledges
+(
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
 );
