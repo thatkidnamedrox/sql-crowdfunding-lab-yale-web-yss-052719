2beeca80bafd66736dc94dc7d625d8deb6a4becd
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 15:44:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -60,7 +60,6 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   fROM projects
   INNER JOIN pledges
   ON projects.id = pledges.project_id
-  GROUP BY projects.title
   HAVING projects.category = 'music'
   ;
   """
