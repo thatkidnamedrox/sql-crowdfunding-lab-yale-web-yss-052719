62c76f2dab597e996fb219298fc4cc9fb0da0650
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 15:04:59 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,6 +1,11 @@
 CREATE TABLE groceries
 (
   id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT,
   name TEXT,
   quantity INTEGER,
   aisle INTEGER
