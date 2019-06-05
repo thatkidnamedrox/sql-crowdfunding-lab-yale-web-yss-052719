1fda27a11226d6fc2e24571afbbc4c752f328166
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-05 15:38:56 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   """
-  SELECT projects.titles
+  SELECT projects.title
   FROM projects
   ;
   """
